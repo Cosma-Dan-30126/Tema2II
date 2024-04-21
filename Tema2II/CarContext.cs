607698e562bc19bf757e2a867bbb7bb59771f19e
Cosma-Dan-30126 <126558035+Cosma-Dan-30126@users.noreply.github.com>
2024-04-21 20:19:39 +0300

    Add project files.

---
+++ Tema2II/CarContext.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Tema2II
+{
+    public class CarContext
+    {
+        private string connectionString;
+
+        public CarContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<Car> GetAllCars() 
+        {
+        List<Car> cars = new List<Car>();
+            using (SqlConnection connection = new SqlConnection(connectionString.Replace("trust server certificate=true;", "")))
+
+
+            {
+
+                string query = "SELECT * FROM Reprezentanta ";
+
+                using (SqlCommand command = new SqlCommand(query,connection)) 
+                {
+                connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read()) 
+                    {
+                        Car car = new Car();
+                        car.Id = Convert.ToInt32(reader["Id"]);
+                        car.Brand = Convert.ToString(reader["Brand"]);
+                        car.Type = Convert.ToString(reader["Type_Car"]);
+                        car.Model = Convert.ToString(reader["Model"]);
+                        car.NumberOfOwners = Convert.ToInt32(reader["Nr_owners"]);
+                        cars.Add(car);
+
+                    }
+                    reader.Close();
+                }
+            }
+            return cars;
+        }
+
+        public void AddCar(Car car)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString.Replace("trust server certificate=true;", "")))
+            {
+                string query = "INSERT INTO Reprezentanta (Brand, Type_Car, Model, Nr_owners) " +
+                               "VALUES (@Brand, @Type, @Model, @NumberOfOwners)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Brand", car.Brand);
+                    command.Parameters.AddWithValue("@Type", car.Type);
+                    command.Parameters.AddWithValue("@Model", car.Model);
+                    command.Parameters.AddWithValue("@NumberOfOwners", car.NumberOfOwners);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        public void UpdateCar(Car car)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString.Replace("trust server certificate=true;", "")))
+            {
+                string query = "UPDATE Reprezentanta SET Brand = @Brand, Type_Car = @Type, " +
+                               "Model = @Model, Nr_owners = @NumberOfOwners WHERE Id = @Id";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Brand", car.Brand);
+                    command.Parameters.AddWithValue("@Type", car.Type);
+                    command.Parameters.AddWithValue("@Model", car.Model);
+                    command.Parameters.AddWithValue("@NumberOfOwners", car.NumberOfOwners);
+                    command.Parameters.AddWithValue("@Id", car.Id);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        public void RemoveCar(int carId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString.Replace("trust server certificate=true;", "")))
+            {
+                string query = "DELETE FROM Reprezentanta WHERE Id = @Id";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", carId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Tema2II/WebService1.asmx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Tema2II
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+     //[System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        private readonly CarContext _carContext;
+
+        public WebService1()
+        {
+            _carContext = new CarContext(ConfigurationManager.ConnectionStrings["CarConnection"].ConnectionString);
+        }
+
+        [WebMethod]
+        public List<Car> GetAllCars()
+        {
+            return _carContext.GetAllCars();
+        }
+
+        [WebMethod]
+        public void AddCar(string brand, string type, string model, int numberOfOwners)
+        {
+            Car newCar = new Car { Brand = brand, Type = type, Model = model, NumberOfOwners = numberOfOwners };
+            _carContext.AddCar(newCar);
+        }
+
+        [WebMethod]
+        public void UpdateCar(int carId, string brand, string type, string model, int numberOfOwners)
+        {
+            Car updatedCar = new Car { Id = carId, Brand = brand, Type = type, Model = model, NumberOfOwners = numberOfOwners };
+            _carContext.UpdateCar(updatedCar);
+        }
+
+
+        [WebMethod]
+        public void RemoveCar(int carId)
+        {
+            _carContext.RemoveCar(carId);
+        }
+
+    }
+}
